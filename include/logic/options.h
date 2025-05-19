b15c1f79550577c5fa658503394af60cf01bbc46
akadata <akadata@akadata.ltd>
2025-05-19 22:33:06 +0000

    started extra options and made argc == 1 give help, best be nice to the one using the application

--- include/logic/options.h
+++ include/logic/options.h
@@ -1,17 +1,17 @@
 /*
  * This file is part of OpenCorsairLink.
  * Copyright (C) 2017-2020  Sean Nelson <audiohacked@gmail.com>
-
+ *
  * OpenCorsairLink is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * any later version.
-
+ *
  * OpenCorsairLink is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
  * along with OpenCorsairLink.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -46,10 +46,7 @@ enum led_modes {
 };
 
 struct temp_speed_pair {
-  /** temperature */
   int8_t temperature;
-
-  /** fan speed PWM */
   int8_t speed;
 };
 
@@ -65,12 +62,12 @@ enum motor_modes {
 };
 
 struct option_flags {
-  // flags
-  // unsigned int debug_messages:1;
   unsigned int set_led : 1;
   unsigned int set_temperature : 1;
   unsigned int set_fan : 1;
   unsigned int set_pump : 1;
+  unsigned int show_status : 1;
+  unsigned int show_json : 1;
 };
 
 struct led_control {
@@ -80,17 +77,15 @@ struct led_control {
   uint16_t temperatures[3];
   uint8_t speed;
   uint8_t count;
-  uint8_t led_type; // commanderpro
-  uint8_t direction; // commanderpro
-  uint8_t change_style; // commanderpro
+  uint8_t led_type;
+  uint8_t direction;
+  uint8_t change_style;
 };
 
 struct fan_control {
   uint8_t channel;
   enum motor_modes mode;
   struct temp_speed_pair table[7];
-
-  // data returns
   uint8_t fan_count;
   uint8_t fan_type;
   uint8_t temp_sensor_id;
@@ -106,8 +101,6 @@ struct pump_control {
   uint8_t channel;
   enum motor_modes mode;
   struct temp_speed_pair table[6];
-
-  // data returns
   uint16_t speed;
   uint8_t speed_pwm;
   uint16_t speed_rpm;
@@ -130,6 +123,8 @@ enum {
   OPTION_FAN,
   OPTION_LED,
   OPTION_PUMP,
+  OPTION_STATUS,
+  OPTION_JSON,
 };
 
 static struct option long_options[] = {
@@ -142,40 +137,28 @@ static struct option long_options[] = {
     {"led", required_argument, 0, OPTION_LED},
     {"fan", required_argument, 0, OPTION_FAN},
     {"pump", required_argument, 0, OPTION_PUMP},
+    {"status", no_argument, 0, OPTION_STATUS},
+    {"json", no_argument, 0, OPTION_JSON},
     {0, 0, 0, 0}};
 
-#define INIT_WARNING_LED(xx)                                                   \
-  xx.red = 0xFF;                                                               \
-  xx.green = 0x00;                                                             \
+#define INIT_WARNING_LED(xx) \
+  xx.red = 0xFF; \
+  xx.green = 0x00; \
   xx.blue = 0x00;
 
-#define INIT_DEFAULT_LED(xx)                                                   \
-  xx.red = 0xFF;                                                               \
-  xx.green = 0xFF;                                                             \
+#define INIT_DEFAULT_LED(xx) \
+  xx.red = 0xFF; \
+  xx.green = 0xFF; \
   xx.blue = 0x00;
 
-#define INIT_RAINBOW_LED(xx)                                                   \
-  xx[0].red = 0xff;                                                            \
-  xx[0].green = 0x00;                                                          \
-  xx[0].blue = 0x00;                                                           \
-  xx[1].red = 0xff;                                                            \
-  xx[1].green = 0x80;                                                          \
-  xx[1].blue = 0x00;                                                           \
-  xx[2].red = 0xff;                                                            \
-  xx[2].green = 0xff;                                                          \
-  xx[2].blue = 0x00;                                                           \
-  xx[3].red = 0x00;                                                            \
-  xx[3].green = 0xff;                                                          \
-  xx[3].blue = 0x00;                                                           \
-  xx[4].red = 0x00;                                                            \
-  xx[4].green = 0x00;                                                          \
-  xx[4].blue = 0xff;                                                           \
-  xx[5].red = 0x4b;                                                            \
-  xx[5].green = 0x00;                                                          \
-  xx[5].blue = 0x82;                                                           \
-  xx[6].red = 0x7f;                                                            \
-  xx[6].green = 0x00;                                                          \
-  xx[6].blue = 0xff;
+#define INIT_RAINBOW_LED(xx) \
+  xx[0].red = 0xff; xx[0].green = 0x00; xx[0].blue = 0x00; \
+  xx[1].red = 0xff; xx[1].green = 0x80; xx[1].blue = 0x00; \
+  xx[2].red = 0xff; xx[2].green = 0xff; xx[2].blue = 0x00; \
+  xx[3].red = 0x00; xx[3].green = 0xff; xx[3].blue = 0x00; \
+  xx[4].red = 0x00; xx[4].green = 0x00; xx[4].blue = 0xff; \
+  xx[5].red = 0x4b; xx[5].green = 0x00; xx[5].blue = 0x82; \
+  xx[6].red = 0x7f; xx[6].green = 0x00; xx[6].blue = 0xff;
 
 enum {
   SUBOPTION_FAN_CHANNEL = 0,
@@ -206,28 +189,31 @@ enum {
   SUBOPTION_PUMP_LIST_END,
 };
 
-static char *fan_options[] = {[SUBOPTION_FAN_CHANNEL] = "channel",
-                              [SUBOPTION_FAN_MODE] = "mode",
-                              [SUBOPTION_FAN_PWM] = "pwm",
-                              [SUBOPTION_FAN_RPM] = "rpm",
-                              [SUBOPTION_FAN_TEMPERATURES] = "temps",
-                              [SUBOPTION_FAN_SPEEDS] = "speeds",
-                              [SUBOPTION_FAN_LIST_END] = 0};
+static char *fan_options[] = {
+  [SUBOPTION_FAN_CHANNEL] = "channel",
+  [SUBOPTION_FAN_MODE] = "mode",
+  [SUBOPTION_FAN_PWM] = "pwm",
+  [SUBOPTION_FAN_RPM] = "rpm",
+  [SUBOPTION_FAN_TEMPERATURES] = "temps",
+  [SUBOPTION_FAN_SPEEDS] = "speeds",
+  [SUBOPTION_FAN_LIST_END] = 0};
 
-static char *led_options[] = {[SUBOPTION_LED_CHANNEL] = "channel",
-                              [SUBOPTION_LED_MODE] = "mode",
-                              [SUBOPTION_LED_SPEED] = "speed",
-                              [SUBOPTION_LED_COLORS] = "colors",
-                              [SUBOPTION_LED_WARN] = "warning_color",
-                              [SUBOPTION_LED_TEMPERATURES] = "temps",
-                              [SUBOPTION_LED_LIST_END] = 0};
+static char *led_options[] = {
+  [SUBOPTION_LED_CHANNEL] = "channel",
+  [SUBOPTION_LED_MODE] = "mode",
+  [SUBOPTION_LED_SPEED] = "speed",
+  [SUBOPTION_LED_COLORS] = "colors",
+  [SUBOPTION_LED_WARN] = "warning_color",
+  [SUBOPTION_LED_TEMPERATURES] = "temps",
+  [SUBOPTION_LED_LIST_END] = 0};
 
-static char *pump_options[] = {[SUBOPTION_PUMP_MODE] = "mode",
-                               [SUBOPTION_PUMP_PWM] = "pwm",
-                               [SUBOPTION_PUMP_RPM] = "rpm",
-                               [SUBOPTION_PUMP_TEMPERATURES] = "temps",
-                               [SUBOPTION_PUMP_SPEEDS] = "speeds",
-                               [SUBOPTION_PUMP_LIST_END] = 0};
+static char *pump_options[] = {
+  [SUBOPTION_PUMP_MODE] = "mode",
+  [SUBOPTION_PUMP_PWM] = "pwm",
+  [SUBOPTION_PUMP_RPM] = "rpm",
+  [SUBOPTION_PUMP_TEMPERATURES] = "temps",
+  [SUBOPTION_PUMP_SPEEDS] = "speeds",
+  [SUBOPTION_PUMP_LIST_END] = 0};
 
 void fan_suboptions_parse(char *subopts, struct fan_control *settings);
 void led_suboptions_parse(char *subopts, struct led_control *settings);
